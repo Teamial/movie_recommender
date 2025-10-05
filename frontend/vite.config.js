36ac34aa097554a7ceded5ecb4cefa6db122fb3f
Teanna Cole <48teanna@gmail.com>
2025-10-05 19:58:23 -0400

    Fix vite config: Add explicit extension resolution for imports

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    extensions: ['.mjs', '.js', '.jsx', '.json', '.ts', '.tsx']
   },
   server: {
     proxy: {
